5dd649817f238b70696ced6760e487f7acc4a1ec
PauliusVizbara <paulius1207@gmail.com>
2019-03-06 23:36:12 +0200

    something,idk

--- connect.php
+++ connect.php
@@ -1,19 +1,41 @@
 <?php
-$servername = 'db4free.net';
-$username = "pauviz1";
-$password = "pauviz1admin";
-$database = "dd_pauliaus";
 
-$conn = @new mysqli($servername, $username, $password, $database);
 
-if ($conn->connect_error) {
-    echo '<div style="width:100%"">
-  <h3>Klaida!</h3>
-  <p>Nepavyko prisijungti prie MySQL.</p>
-</div>';
+function establishConnection($type)
+{
+
+    $username = "pauviz1";
+    $database = "dd_pauliaus";
+
+
+   if ($type === "global") {
+       $servername = 'db4free.net';
+       $password = "pauviz1admin";
+    }
+   else if ($type === "local") {
+        $servername = 'localhost';
+        $username = "root";
+        $password = "";
+    }
+
+    $connection = @new mysqli($servername, $username, $password, $database);
+    if ($connection->connect_error) {
+        echo '<div style="width:100%"">
+        <h3>Klaida!</h3>
+         <p>Nepavyko prisijungti prie MySQL.</p>
+        </div>';
+    }
+    return $connection;
+
+   /* if ($conn->connect_error) {
+        echo '<div style="width:100%"">
+        <h3>Klaida!</h3>
+         <p>Nepavyko prisijungti prie MySQL.</p>
+        </div>';
+
+    }*/
+
 
 }
-
-
 
 ?>
