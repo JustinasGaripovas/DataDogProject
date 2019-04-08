0f7ae9b82a57e51e360c0c123562ed865d249c12
Paulius <paulius1207@gmail.com>
2019-04-08 22:09:13 +0300

    IKI REBASE

--- DataDog/src/Migrations/Version20190408190635.php
+++ DataDog/src/Migrations/Version20190408190635.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190323101042 extends AbstractMigration
+final class Version20190408190635 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,8 @@ final class Version20190323101042 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, text VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('DROP TABLE test_entity2');
+        $this->addSql('ALTER TABLE event DROP lat, DROP lng');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +31,7 @@ final class Version20190323101042 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE comment');
+        $this->addSql('CREATE TABLE test_entity2 (id INT AUTO_INCREMENT NOT NULL, test VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE event ADD lat DOUBLE PRECISION DEFAULT NULL, ADD lng DOUBLE PRECISION DEFAULT NULL');
     }
 }
