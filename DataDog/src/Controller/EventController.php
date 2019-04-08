b7cccb9121d55618f7adf88c14ae55611641ffad
Paulius <paulius1207@gmail.com>
2019-04-08 17:46:30 +0300

    Random tries

--- DataDog/src/Controller/EventController.php
+++ DataDog/src/Controller/EventController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class EventController extends Controller
+class EventController extends AbstractController
 {
     /**
      * @Route("/", name="event_index")
