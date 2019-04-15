aba93a65edcd9e1f1673249b832e01472eb141e8
Rokas <rokisdhk@gmail.com>
2019-04-15 15:48:12 +0300

    Basic and not pretty comments added to the events and uploaded to and from DB

--- DataDog/src/Entity/Event.php
+++ DataDog/src/Entity/Event.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
@@ -84,9 +85,15 @@ class Event
      */
     private $eventCategories;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="event", orphanRemoval=true)
+     */
+    private $comments;
+
     public function __construct()
     {
         $this->eventCategories = new ArrayCollection();
+        $this->comments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -199,6 +206,37 @@ class Event
     public function getEventCategories()
     {
         return $this->eventCategories;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setEvent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getEvent() === $this) {
+                $comment->setEvent(null);
+            }
+        }
+
+        return $this;
     }
 
 }
