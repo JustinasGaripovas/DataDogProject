eb0120b65325cbf47d8db1f0ce50f1d6a37743dc
Jelena <j.cernysova@gmail.com>
2019-04-19 01:53:21 +0300

    Make user admin and remove any user

--- DataDog/src/Controller/UsersController.php
+++ DataDog/src/Controller/UsersController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
 
 class UsersController extends AbstractController
 {
     /**
-     * @Route("/users", name="users")
+     * @Route("/users", name="users_index")
      */
     public function index(UserRepository $repository)
     {
@@ -20,5 +22,34 @@ class UsersController extends AbstractController
         return $this->render('users/index.html.twig', [
             'users' => $users,
         ]);
+    }
+
+    /**
+     * @Route("/users/{id}/delete", name="user_delete")
+     */
+    public function deleteUser(Request $request, User $user)
+    {
+
+        $this->denyAccessUnlessGranted("ROLE_ADMIN");
+        $em = $this->getDoctrine()->getManager();
+        if(in_array("ROLE_ADMIN", $user->getRoles() )) {
+            $em->remove($user);
+        }
+        $em->flush();
+        return $this->redirectToRoute('users_index');
+    }
+
+    /**
+     * @Route("/users/{id}/makeadmin", name="user_to_admin")
+     */
+    public function userToAdmin(Request $request, User $user)
+    {
+
+        $this->denyAccessUnlessGranted("ROLE_ADMIN");
+        $em = $this->getDoctrine()->getManager();
+        //$em->remove($user);
+        $user->SetRoles(array('ROLE_ADMIN'));
+        $em->flush();
+        return $this->redirectToRoute('users_index');
     }
 }
