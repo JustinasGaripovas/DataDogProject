ecff5892129f6ab0ea746841978923d57cd0125a
Paulius <paulius1207@gmail.com>
2019-05-06 22:16:31 +0300

    Removed env

---
+++ DataDog/src/Controller/EmailController.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class EmailController extends AbstractController
+{
+
+    /**
+     * @Route("/resetPassword/{token}", name="create_password")
+     */
+    public function createPasswordFromReset(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+
+        $reset_token = $routeParams = $request->attributes->get('token');;
+        $em = $this->getDoctrine()->getManager();
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $entityManager->getRepository(User::class)->findByResetToken($reset_token);
+
+
+        dump($user);
+
+        $defaultData = [];
+        $form = $this->createFormBuilder($defaultData)
+
+            ->add('password', RepeatedType::class, array(
+                'first_options'  => ['label' => 'Naujas slaptažodis'],
+                'second_options' => ['label' => 'Pakartoti naują slaptažodį'],
+                'type' => PasswordType::class,
+                'invalid_message' => 'Nesutinka slaptažodžiai',
+                'options' => array(
+                    'attr' => array(
+                        'class' => 'password-field'
+                    )
+                ),
+                'required' => true,
+                'mapped' => false
+            ))
+            ->add('Update password', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+
+            $passwordEncoder = $encoder;
+
+
+
+            $user->setResetToken(null);
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+
+            $em->flush();
+
+        }
+
+
+        return $this->render('email/create.html.twig', [
+            'controller_name' => $reset_token,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/resetPassword", name="reset_form")
+     */
+    public function resetRequest(Request $request, \Swift_Mailer $mailer)
+    {
+
+
+        $defaultData = [];
+        $form = $this->createFormBuilder($defaultData)
+            ->add('email', EmailType::class)
+            ->add('Reset password', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+
+
+            $this->initializePasswordReset($data['email'],$mailer);
+        }
+
+        return $this->render('email/reset.html.twig', [
+            'controller_name' => 'EmailController',
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    public function initializePasswordReset($email, \Swift_Mailer $mailer)
+    {
+        $reset_token = $this->generateRandomString();
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $entityManager->getRepository(User::class)->findByEmail($email);
+
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'No product found for id'
+            );
+        }
+
+        $user->setResetToken($reset_token);
+        $entityManager->flush();
+
+
+        $message = (new \Swift_Message('Hello Email'))
+            ->setFrom('datadog.ktu@gmail.com')
+            ->setTo($email)
+            ->setBody(
+                $this->renderView(
+                // templates/emails/registration.html.twig
+                    'email/reset_confirmation_email.html.twig',
+                    ['email' => $email]
+                ),
+                'text/html'
+            )
+
+        ;
+
+        $mailer->send($message);
+
+        //return $this->render(...);
+    }
+
+    function generateRandomString($length = 15) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+
+
+
+}
--- DataDog/src/Repository/UserRepository.php
+++ DataDog/src/Repository/UserRepository.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace App\Repository;
-
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
-
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
  * @method User|null findOneBy(array $criteria, array $orderBy = null)
@@ -18,17 +15,14 @@ class UserRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, User::class);
     }
-
     public function findAll()
     {
         return $this->createQueryBuilder('u')
-
             ->orderBy('u.username', 'ASC')
             ->getQuery()
             ->getResult()
             ;
     }
-
     // /**
     //  * @return User[] Returns an array of User objects
     //  */
@@ -46,15 +40,24 @@ class UserRepository extends ServiceEntityRepository
     }
     */
 
-    /*
-    public function findOneBySomeField($value): ?User
+    public function findByResetToken($value): ?User
     {
         return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
+            ->andWhere('u.reset_token= :reset_token')
+            ->setParameter('reset_token', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+            ;
+    }
+
+    public function findByEmail($value): ?User
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.email = :email')
+            ->setParameter('email', $value)
             ->getQuery()
             ->getOneOrNullResult()
         ;
     }
-    */
+
 }
