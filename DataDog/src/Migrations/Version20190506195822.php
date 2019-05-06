aeb2a920b1bc67165632f1172a7118671071b46f
Paulius <paulius1207@gmail.com>
2019-05-06 22:58:54 +0300

    Rebase migrations

--- DataDog/src/Migrations/Version20190506195822.php
+++ DataDog/src/Migrations/Version20190506195822.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190428110700 extends AbstractMigration
+final class Version20190506195822 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20190428110700 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE event ADD date DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD reset_token VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20190428110700 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE event DROP date');
+        $this->addSql('ALTER TABLE user DROP reset_token');
     }
 }
